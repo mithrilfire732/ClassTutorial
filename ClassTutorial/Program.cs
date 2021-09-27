c1b9b6c4a8c01ca8264f6858adad802d52fb94c7
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-27 14:15:32 -0400

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Fred = new Student();
+            Fred.Name = "Fred";
+            Fred.SAT = 1000;
+            var Wilma = new Student();
+            Wilma.Name = "Wilma";
+            Wilma.SAT = 2000;
+            var Barney = new Student();
+            Barney.Name = "Barney";
+            Barney.SAT = 1200;
+            Wilma.Print();
+            Fred.Print();
+            Barney.Print();
+        }
+    }
+}
---
+++ ClassTutorial/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTutorial
+{
+   class Student
+    {
+        public string Name { get; set; }
+
+        private int _SAT;
+        public int SAT {
+            get { return _SAT; }
+            set {
+                if(value <400 || value > 1600)
+                {
+                    Console.WriteLine("Invalid SAT Score");
+
+                }
+                else
+                {
+                    this._SAT = value;
+                }
+            } 
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.Name}, {this.SAT}");
+        }
+    }
+}
